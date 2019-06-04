e2adfe64f60aefe5a3792fd78a0852002c5c1b31
SashaOnoshko <onoshkosasha99@gmail.com>
2019-06-05 00:35:30 +0300

    Implemented basic functionality.

--- main.js
+++ main.js
@@ -1,52 +1,78 @@
 
 class Knowledge {
-    constructor(){
-
+    constructor(level){
+        this.level = level;
     }
 }
 
 class Student extends Knowledge{
-    constructor() {
-        super();
+    constructor(studentName, level) {
+        super(level);
+        this.studentName = studentName;
 
     }
-    setKnowledge(){
+
+    setKnowledge(level){
 
     }
 }
 
 class University extends Student{
-    constructor(){
-        super();
+    constructor(name){
+        super(name);
+        this.studentList=[];
 
     }
     setStudent(){
 
     }
-    addStudent(){
+    addStudent(studentName){
+        this.studentList.push(studentName);
+    }
+    calculateGPA(){ //GPA - grade point average
+        let sum=0;
+
+        for(let i=0; i<=this.studentList.length; i++){
+            sum+=i;
+        }
+        return sum / this.studentList.length;
 
     }
 }
 
 class Internship extends Student{
-    constructor(){
+    constructor(name){
         super();
+        this.name=name;
+        this.suitableStudent=[];
+
     }
     setStudent(){
 
     }
     getStudent(){
+        let gpa = university1.calculateGPA();
+        university1.studentList.forEach((student)=>{
+            if(student.level>gpa){
+                this.suitableStudent.push(student.studentName);
 
+            }
+        });
+
+        return this.suitableStudent;
     }
 }
 
 let university1 = new University('CHSBC');
 
-university1.addStudent(new Student("Andrew Kostenko"));
-university1.addStudent(new Student("Julia Veselkina"));
-university1.addStudent(new Student("Maria Perechrest"));
+university1.addStudent(new Student('Sasha Onoshko', '5'));
+university1.addStudent(new Student("Andrew Kostenko", '4'));
+university1.addStudent(new Student("Julia Veselkina",'3'));
+university1.addStudent(new Student("Maria Perechrest", '2'));
+
 
 let internship = new Internship('Interlink');
+
 
 console.log('List of internship\'s students:');
 console.log(internship.getStudent());
